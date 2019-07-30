d0b59a0cc89e35f02bc9309e0d2f99d0bc916fd4
Анастасия <na.konsta@gmail.com>
2019-07-31 00:26:50 +0300

    Booking

--- portfolio-four/js/slow_scroll.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('.top').bind("click",function(e){var anchor=$(this);$('html, body').stop().animate({scrollTop:$(anchor.attr('href')).offset().top},1000);e.preventDefault();});});
