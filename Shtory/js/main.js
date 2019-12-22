098c9a428289d7880b4f57b75fe2e926f876a04a
Анастасия Константинович <nastia@MacBook-Air-Anastasia.local>
2019-12-23 01:25:02 +0300

    Mainpage desktop is ready, adaptive in process

--- Shtory/js/main.js
+++ Shtory/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
+    // Modals for 'Our works'
     $('.fancy').fancybox();
+
+    // Copywrite year
+
+    var today = new Date();
+    $('.footer__copywrite-year').text(today.getFullYear());
+
 });
